f91416fd91f9f53a0a6ace1ba848dbb586a9361d
Nahuel Pierini <Nahuel.Pierini90@hotmail.com>
2023-11-15 21:12:25 -0300

    back end CRUD user

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,9 @@
 const expressAsyncHandler = require('express-async-handler');
 const User= require('../Models/userModel');
-const asyncHandler =require('express-async-handler')
+const asyncHandler =require('express-async-handler');
+const { generateToken } = require('../Config/jwtToken');
+const { parse } = require('dotenv');
+const { param } = require('../Routes/authRoute');
 const createUser= asyncHandler( async (req, res, next) =>{
     const email= req.body.email;
     const findUser= await User.findOne({email:email});
@@ -24,11 +27,78 @@ const loginUserCtrl= asyncHandler(async(req, res)=>{
     //check if user exists or not 
     const findUser= await User.find({email});
     if(findUser && await findUser.isPasswordMatched(password)) {
-        res.json(findUser)
+        res.json({
+            _id : findUser?._id,
+            firstName: findUser?.firstName,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+            })
     }else{
         throw new Error("Inavalid Credentials")
     }
 
+});
+//Update a user
+
+const updateUser= asyncHandler(async(req, res)=>{
+    const {id}= req.params._id;
+    try {
+        const getUser= await User.findByIdAndUpdate(id, {
+            _id : req?._id,
+            firstName: req?.firstName,
+            email: req?.email,
+            mobile: req?.mobile,
+        }, 
+        { 
+            new: true,
+        });
+        res.json({
+            updateUser});
+    } catch (error) {
+        throw new Error(error);
+    }
 })
 
-module.exports={createUser, loginUserCtrl}
+
+
+// Get all users
+
+const getaUser = asyncHandler( async(req, res)=>{
+  
+  try {
+    const getUsers= await User.find();
+    res.json({
+        getUsers});
+  } catch (error) {
+    throw new Error(error)
+  }
+});
+
+//Get Single User
+
+const getUser= asyncHandler(async(req, res)=>{
+    const {id}= req.params._id;
+    try {
+        const getUser= await User.findById(id);
+        res.json({
+            getUser});
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+//Delete User
+
+const delUser= asyncHandler(async(req, res)=>{
+    const {id}= req.params._id;
+    try {
+        const delUser= await User.findByIdAndDelete(id);
+        res.json({
+            delUser});
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+module.exports={createUser, loginUserCtrl,getaUser, getUser, delUser, updateUser}
--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,8 +1,13 @@
 const express= require('express');
-const { createUser, loginUserCtrl } = require('../Controllers/userController');
+const { createUser, loginUserCtrl, getUser, delUser, updateUser } = require('../Controllers/userController');
 const router= express.Router();
 
-
+//routes user
 router.post('/register', createUser);
-router.post('/login', loginUserCtrl )
+router.post('/login', loginUserCtrl );
+router.get('/all-users, getaUser');
+router.get('/:id', getUser);
+router.delete('/delete/:id', delUser);
+router.put('/updae/:id', updateUser);
+
 module.exports= router;
